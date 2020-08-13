fb20be7d9db4567a266bcf48e73bacad0b2de617
zhydani <zhydani@gmail.com>
2020-08-13 09:12:05 -0300

    hello world configurado e presistence funcionando

---
+++ Contabilidade/src/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.io.Serializable;
+
+import javax.inject.Named;
+
+@Named
+
+public class HelloWorldController implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4585283208089720699L;
+
+}
