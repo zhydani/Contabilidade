ab4fff34c67d2401591ffc94f4291e15ebdc6a16
zhydani <zhydani@gmail.com>
2020-08-13 21:03:34 -0300

    alteração layout

---
+++ Contabilidade/src/controller/IndexController.java
@@ -0,0 +1,17 @@
+package controller;
+
+
+import java.io.Serializable;
+
+import javax.inject.Named;
+
+@Named
+
+public class IndexController implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4585283208089720699L;
+
+}
